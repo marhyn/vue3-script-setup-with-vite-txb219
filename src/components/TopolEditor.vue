baf4d852d11786419ba096757bbf617463536793
marhyn <mareshynek2@gmail.com>
2023-05-31 14:30:26 +0200

    Ugha bunga

--- src/components/TopolEditor.vue
+++ src/components/TopolEditor.vue
@@ -1,7 +1,23 @@
 <script setup>
 import { TopolEditor, TopolPlugin } from '@topol.io/editor-vue'
+
+const topolOptions = {
+  id: '#app',
+  authorize: {
+    apiKey: 'd4U1ZDySMKUfF8wKPcyN2pwo9WFxvAFNWiBp51x09so8RHFc7mnynzYU344H',
+    userId: 'userId'
+  }
+}
+
+const onInit = () => {
+  fetch('https://tlapi.github.io/topol-editor/templates/1.json')
+    .then((response) => response.text())
+    .then((template) => {
+      //Load the template in Editor
+      TopolPlugin.load(template);
+    });}
 </script>
 
 <template>
-  <TopolEditor />
+  <TopolEditor :options="topolOptions" @on-init="onInit" />
 </template>
